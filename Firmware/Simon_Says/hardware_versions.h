922f54d9682d7645bf32ec7b8ea88b7c5d593d49
Jed Parsons <jedp@me.com>
2015-05-02 15:24:28 -0700

    Board def for duemilanove for breadboard prototyping

--- Firmware/Simon_Says/hardware_versions.h
+++ Firmware/Simon_Says/hardware_versions.h
@@ -15,9 +15,30 @@
 
 
 // Uncomment one of the following, corresponding to the board you have.
-#define BOARD_REV_2_3_2011 //Works with PTH board label '2-3-2011'
-//#define BOARD_REV_4_9_2009   //Works with SMD simoon board label '4/9/2009' and '9/7/11'
+#define BOARD_DUEMILANOVE // For breadboard prototyping and experimentation
+//#define BOARD_REV_2_3_2011 //Works with PTH board label '2-3-2011'
+//#define BOARD_REV_4_9_2009  //Works with SMD simoon board label '4/9/2009' and '9/7/11'
 //#define BOARD_REV_6_25_2008
+
+#ifdef BOARD_DUEMILANOVE
+  // Puts blue LED in pin 11 so system LED on pin 13 doesn't flash as well.
+  // LED pin definitions, these are Arduino pins, not ATmega pins
+  #define LED_RED     10
+  #define LED_GREEN   3
+  #define LED_BLUE    11
+  #define LED_YELLOW  5
+
+  // Button pin definitions
+  #define BUTTON_RED    9
+  #define BUTTON_GREEN  2
+  #define BUTTON_BLUE   12
+  #define BUTTON_YELLOW 6
+
+  // Buzzer pin definitions
+  #define BUZZER1  4
+  #define BUZZER2  7
+
+#endif
 
 #ifdef BOARD_REV_2_3_2011
   // LED pin definitions, these are Arduino pins, not ATmega pins
@@ -25,13 +46,13 @@
   #define LED_GREEN   3
   #define LED_BLUE    13
   #define LED_YELLOW  5
-  
+
   // Button pin definitions
   #define BUTTON_RED    9
   #define BUTTON_GREEN  2
   #define BUTTON_BLUE   12
   #define BUTTON_YELLOW 6
-  
+
   // Buzzer pin definitions
   #define BUZZER1  4
   #define BUZZER2  7
@@ -43,13 +64,13 @@
   #define LED_YELLOW   5 //PORTD.5
   #define LED_RED      10 //PORTB.2
   #define LED_GREEN    2 //PORTD.2
-  
+
   // Button pin definitions
   #define BUTTON_RED     8 //PINB.0
   #define BUTTON_GREEN   9 //PINB.1
   #define BUTTON_BLUE    7 //PIND.7
   #define BUTTON_YELLOW  6 //PIND.6
-  
+
   // Buzzer pin definitions
   #define BUZZER1  3 //PORTD.3
   #define BUZZER2  4 //PORTD.4
@@ -61,13 +82,13 @@
   #define LED_GREEN   2 //PORTD.2
   #define LED_BLUE    A0 //PORTC.0
   #define LED_YELLOW  5 //PORTD.5
-  
+
   // Button pin definitions
   #define BUTTON_RED     A2 //PINC.2
   #define BUTTON_GREEN   A5 //PINC.5
   #define BUTTON_BLUE    A1 //PINC.1
   #define BUTTON_YELLOW  6 //PIND.6
-  
+
   // Buzzer pin definitions
   #define BUZZER1 3 //PORTD.3
   #define BUZZER2 4 //PORTD.4
